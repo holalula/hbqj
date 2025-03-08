4596ecac1f6d28442be09e92e9c4275c42b2b1f7
holalula <keepholo@gmail.com>
2025-03-08 21:21:02 +0800

    test: add test

---
+++ test/test.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "../HaiBinQiangJia/include/ff/FFProcess.h"
+#include "../HaiBinQiangJia/include/utils/sigscanner.h"
+#include "../HaiBinQiangJia/HBQJ_Process.h"
+
+using namespace std;
+
+int main()
+{
+    std::cout << "HaiBinQiangJia\n";
+   
+}
