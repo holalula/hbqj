0e35f3bf2a5520c55f9a19e3f2e19a2642ae6d37
holalula <keepholo@gmail.com>
2025-05-03 18:09:11 +0800

    test: validate msvc exception handling model

--- client/src/ui/debug_window.h
+++ client/src/ui/debug_window.h
@@ -1,11 +1,42 @@
 #pragma once
 
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 #include "ui.h"
 
 namespace hbqj {
     char parameter_1[32] = "0x1234";
+
+    using json = nlohmann::json;
+
+    void CatchExceptions() {
+        // If /EHs is not added in MSVC (which means the stack unwinding is not enabled),
+        // The first runtime exception can be caught, while the json exception can't.
+        // The second invalid json exception causes the process to exit and the error message is
+        // > Process finished with exit code -1073740791 (0xC0000409)
+        // 0xC0000409 represents STATUS_STACK_BUFFER_OVERRUN, which is usually triggered by
+        // a stack buffer overflow (such as array out of range), and this could be caused by no stack unwinding.
+
+        if (ImGui::Button("Runtime Exception")) {
+            try {
+                throw std::runtime_error("ex");
+            } catch (...) {
+                log("Catch ex");
+            }
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Json Exception")) {
+            try {
+                std::string invalid_json = "{\"key\": invalid}";
+                auto j = json::parse(invalid_json);
+            } catch (...) {
+                log("Catch json ex");
+            }
+        }
+    }
 
     void DebugWindow() {
         ImGui::SetNextWindowSize({400, 400});
@@ -34,6 +65,8 @@ namespace hbqj {
                 std::cout << "Invalid hex number" << std::endl;
             }
         }
+
+        CatchExceptions();
 
         ImGui::End();
     }
